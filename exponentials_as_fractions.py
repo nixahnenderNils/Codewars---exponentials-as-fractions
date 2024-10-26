88c68bb261fbfa315483b505c50d2de779b02e61
nixahnenderNils <nils.kindt@posteo.de>
2024-10-26 11:17:17 +0200

    Update exponentials_as_fractions.py
    This version was previously in my codewars-account. 
    
    Maybe it is simply an older Version.

--- exponentials_as_fractions.py
+++ exponentials_as_fractions.py
@@ -1,13 +1,3 @@
-def test(number, length):
-    if number <= 10**length:
-        return True
-
-def ggt(a, b):
-    if b==0:
-        return a
-    else:
-        return ggt(b, a%b)
-
 def expand(x, digits):
     denominator = 1
     numerator = 1
@@ -25,11 +15,4 @@ def expand(x, digits):
         if (denominator*counter+x**counter)/fac2 >= 10**(digits):
             break
     
-    return [int(denominator/fac1), int(numerator/fac1), denominator/numerator]
-
-if __name__ == '__main__':
-
-    print(expand(1,2))
-    print(expand(2, 5))
-    print(expand(3, 10))
-        
+    return [int(denominator/fac1), int(numerator/fac1)]
