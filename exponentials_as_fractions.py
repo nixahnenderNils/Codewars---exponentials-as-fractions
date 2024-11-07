f1e0474afaff4eefd333196ac1b18d07019104a2
Jannek <no.mail@gmail.com>
2024-11-07 14:50:48 +0100

    Changed a lot

--- exponentials_as_fractions.py
+++ exponentials_as_fractions.py
@@ -1,35 +1,24 @@
-def test(number, length):
-    if number <= 10**length:
-        return True
-
-def ggt(a, b):
-    if b==0:
-        return a
-    else:
-        return ggt(b, a%b)
-
 def expand(x, digits):
     denominator = 1
     numerator = 1
     counter = 1
     
+    fac = lambda a: 1 if a <= 1 else a*fac(a-1)
+    ggt = lambda a,b: a if b==0 else ggt(b, a%b)
+
     while True: 
-        denominator = denominator*counter
-        denominator += x**counter
-        numerator = numerator*counter
-        counter +=1
+        numerator=(x**counter*denominator+numerator*fac(counter))
+        denominator=denominator*fac(counter)
+        if len(str(int(numerator/ggt(numerator, denominator)))) >=digits:
+             break
+        counter+=1
         
-        ggt = lambda a,b: a if b==0 else ggt(b, a%b)
-        fac1 = ggt(denominator, numerator)
-        fac2 = ggt(denominator*counter+x**counter, numerator*counter)
-        if (denominator*counter+x**counter)/fac2 >= 10**(digits):
-            break
-    
-    return [int(denominator/fac1), int(numerator/fac1), denominator/numerator]
+    return [int(numerator/ggt(numerator, denominator)), int(denominator/ggt(numerator, denominator)), numerator/denominator]
 
 if __name__ == '__main__':
 
     print(expand(1,2))
     print(expand(2, 5))
     print(expand(3, 10))
+    print(expand(1.5,10))
         
